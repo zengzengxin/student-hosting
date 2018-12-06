fbc4fbbbb13104b360da449bddd8442011431a69
huanglp <huanglongpeng@talkmoney.cn>
2018-12-06 15:11:04 +0800

    feat all : 在CommonConfig中添加localDateTimeToTimestampConverter方法

--- dao/src/main/java/com/luwei/config/CommonConfig.java
+++ dao/src/main/java/com/luwei/config/CommonConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.TimeZone;
 
 @Configuration
 public class CommonConfig {
@@ -18,18 +20,18 @@ public class CommonConfig {
         return Predicates.alwaysTrue();
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedHeaders("*")
-//                        .allowedMethods("*")
-//                        .allowedOrigins("*");
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 
     @Bean
     public Converter<String, Date> dateToTimestampConverter() {
@@ -37,6 +39,17 @@ public class CommonConfig {
             @Override
             public Date convert(String source) {
                 return new Date(Long.parseLong(source, 10));
+            }
+        };
+    }
+
+    @Bean
+    public Converter<String, LocalDateTime> localDateTimeToTimestampConverter() {
+        return new Converter<String, LocalDateTime>() {
+            @Override
+            public LocalDateTime convert(String source) {
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(source, 10)),
+                        TimeZone.getDefault().toZoneId());
             }
         };
     }
