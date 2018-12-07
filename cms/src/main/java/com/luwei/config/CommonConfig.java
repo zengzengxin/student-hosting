62e5c81c7aa017e864be855ff1856b002fb50c1c
huanglp <huanglongpeng@talkmoney.cn>
2018-12-07 17:50:25 +0800

    fix all : 修改CommonConfig, GET请求仍需添加转换器

--- cms/src/main/java/com/luwei/config/CommonConfig.java
+++ cms/src/main/java/com/luwei/config/CommonConfig.java
@@ -4,8 +4,13 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @Configuration
 public class CommonConfig {
@@ -27,16 +32,25 @@ public class CommonConfig {
         };
     }
 
-    // Spring Boot 2.0 后以下的转换器暂时不需要
     /*@Bean
     public Converter<String, Date> dateToTimestampConverter() {
-        return source -> new Date(Long.parseLong(source, 10));
-    }
+        return new Converter<String, Date>() {
+            @Override
+            public Date convert(String source) {
+                return new Date(Long.parseLong(source, 10));
+            }
+        };
+    }*/
 
     @Bean
     public Converter<String, LocalDateTime> localDateTimeToTimestampConverter() {
-        return source -> LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(source, 10)),
+        return new Converter<String, LocalDateTime>() {
+            @Override
+            public LocalDateTime convert(String source) {
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(source, 10)),
                         TimeZone.getDefault().toZoneId());
-    }*/
+            }
+        };
+    }
 
 }
