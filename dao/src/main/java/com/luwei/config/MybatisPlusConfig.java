436063916d6292a81e45104df91c5c1297288112
huanglp <huanglongpeng@talkmoney.cn>
2018-12-09 16:18:21 +0800

    fix all : 去除MybatisPlusConfig的@EnableTransactionManagement注解

--- dao/src/main/java/com/luwei/config/MybatisPlusConfig.java
+++ dao/src/main/java/com/luwei/config/MybatisPlusConfig.java
@@ -7,7 +7,6 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author miemie
@@ -15,7 +14,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @MapperScan("com.luwei.model")
-@EnableTransactionManagement
 public class MybatisPlusConfig {
     /**
      * 分页插件
