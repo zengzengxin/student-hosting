2bc76f9e3d3c1ece35429d06d0f7abef7011d393
huanglp <huanglongpeng@talkmoney.cn>
2018-12-11 14:06:51 +0800

    feat all : 新增SQL执行效率插件,只在dev/test环境开启

--- cms/src/main/java/com/luwei/CmsApplication.java
+++ cms/src/main/java/com/luwei/CmsApplication.java
@@ -1,15 +1,8 @@
 package com.luwei;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.converter.Converter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.TimeZone;
 
 /**
  * <p>
@@ -18,7 +11,6 @@ import java.util.TimeZone;
  **/
 @EnableSwagger2
 @SpringBootApplication
-@MapperScan("com.luwei.mapper")
 public class CmsApplication {
 
     public static void main(String[] args) {
--- dao/src/main/java/com/luwei/config/MybatisPlusConfig.java
+++ dao/src/main/java/com/luwei/config/MybatisPlusConfig.java
@@ -4,17 +4,16 @@ import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-/**
- * @author miemie
- * @since 2018-08-10
- */
 @Configuration
 @MapperScan("com.luwei.model")
 public class MybatisPlusConfig {
+
     /**
      * 分页插件
      */
@@ -43,5 +42,15 @@ public class MybatisPlusConfig {
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor() {
         return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * SQL执行效率插件
+     * 设置 dev test 环境开启
+     */
+    @Bean
+    @Profile({"dev", "test"})
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 }
