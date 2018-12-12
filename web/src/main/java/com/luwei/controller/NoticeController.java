889c10fa2d6970d451b6e8ac057bc6ba2e967daf
zx <cengzengxin@talkmoney.cn>
2018-12-12 19:21:36 +0800

    fix all : 1212晚上提交，提交web-notice模块

--- web/src/main/java/com/luwei/controller/NoticeController.java
+++ web/src/main/java/com/luwei/controller/NoticeController.java
@@ -1,8 +1,17 @@
 package com.luwei.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.luwei.model.notice.Notice;
+import com.luwei.model.notice.pojo.cms.NoticeQueryDTO;
+import com.luwei.model.notice.pojo.cms.NoticeVO;
+import com.luwei.service.notice.NoticeService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * @author ffq
@@ -13,21 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/notice")
 public class NoticeController {
 
-    //@Resource
-    //private NoticeService iNoticeService;
+    @Resource
+    private NoticeService iNoticeService;
 
 
-/*    @GetMapping
+    @GetMapping
     @ApiOperation("通过id查询一条公告")
-    public ParentVO findById(@RequestParam @ApiParam("noticeId") Integer id) {
-        return NoticeService.getNotice(id);
-    }*/
+    public NoticeVO findById(@RequestParam @ApiParam("noticeId") Integer id) {
+        return iNoticeService.findById(id);
+    }
 
 
-    /*@GetMapping("/page")
+    @GetMapping("/page")
     @ApiOperation("分页")
     public IPage<NoticeVO> page(@ModelAttribute NoticeQueryDTO noticeQueryDTO,Page<Notice> page) {
         return iNoticeService.getNoticePage(page,noticeQueryDTO);
-    }*/
+    }
 }
 
--- web/src/main/java/com/luwei/service/notice/NoticeService.java
+++ web/src/main/java/com/luwei/service/notice/NoticeService.java
@@ -1,8 +1,15 @@
 package com.luwei.service.notice;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.luwei.common.exception.MessageCodes;
+import com.luwei.common.util.BeanUtils;
 import com.luwei.model.notice.Notice;
 import com.luwei.model.notice.NoticeMapper;
+import com.luwei.model.notice.pojo.cms.NoticeQueryDTO;
+import com.luwei.model.notice.pojo.cms.NoticeVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -19,33 +26,33 @@ import org.springframework.stereotype.Service;
 public class NoticeService extends ServiceImpl<NoticeMapper, Notice> {
 
 
-    /*private NoticeVO toNoticeVO(Notice notice) {
+    private NoticeVO toNoticeVO(Notice notice) {
         NoticeVO noticeVO = new NoticeVO();
         BeanUtils.copyNonNullProperties(notice,noticeVO);
         return  noticeVO;
-    }*/
+    }
 
-/*    private Course findById(Integer id) {
+   public  NoticeVO findById(Integer id) {
         // 若此id已被逻辑删除,也会返回null
-        Course course = getById(id);
-        Assert.notNull(course, MessageCodes.COURSE_IS_NOT_EXIST);
-        return course;
-    }*/
+        Notice notice = getById(id);
+        Assert.notNull(notice, MessageCodes.NOTICE_IS_NOT_EXIST);
+        return toNoticeVO(notice);
+    }
 
 
 
 
-    /*public IPage<NoticeVO> getNoticePage( Page<Notice> page,NoticeQueryDTO noticePageDTO) {
+    public IPage<NoticeVO> getNoticePage( Page<Notice> page,NoticeQueryDTO noticePageDTO) {
         return baseMapper.getNoticePage(page,noticePageDTO);
         //TODO---这个方法没有测
-       *//* Notice notice = new Notice();
+       /* Notice notice = new Notice();
         BeanUtils.copyNonNullProperties(noticePageDTO, notice);
         QueryWrapper<Notice> noticeQueryWrapper = new QueryWrapper<Notice>();
        // if(noticePageDTO.)
         noticeQueryWrapper.lambda().between(Notice::getCreateTime,noticePageDTO.getStartTime(), noticePageDTO.getEndTime())
                 .like(Notice::getNoticeTittle,noticePageDTO.getNoticeTittle())
                 .eq(Notice::getNoticeStatus,noticePageDTO.getNoticeStatus());
-        return ConversionBeanUtils.conversionBean(baseMapper.selectPage(page,noticeQueryWrapper), this::toNoticeVO);*//*
-    }*/
+        return ConversionBeanUtils.conversionBean(baseMapper.selectPage(page,noticeQueryWrapper), this::toNoticeVO);*/
+    }
 
 }
