2e89db8a3ac9702545bebbbb7c6f364f4e7d036c
huanglp <huanglongpeng@talkmoney.cn>
2018-12-24 16:48:12 +0800

    feat all : 修改RoleEnum

--- common/src/main/java/com/luwei/common/constant/RoleEnum.java
+++ common/src/main/java/com/luwei/common/constant/RoleEnum.java
@@ -1,25 +1,38 @@
 package com.luwei.common.constant;
 
+import com.baomidou.mybatisplus.core.enums.IEnum;
+
 /**
  * <p>超管操作对象：普通管理员、运营者，普通管理员操作对象：运营者
  *
  * @author luwei
  **/
-public enum RoleEnum {
+public enum RoleEnum implements IEnum<Integer> {
 
     /**
      * 超管ROOT，拥有最高权限
      */
-    ROOT,
+    ROOT(0),
 
     /**
      * 普通管理员ADMIN，拥有部分权限，但比超管权限低
      */
-    ADMIN,
+    ADMIN(1),
 
     /**
      * 运营者OPERATOR，拥有部分权限，但比普通管理员权限低
      */
-    OPERATOR
+    OPERATOR(2);
+
+    private int value;
+
+    RoleEnum(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public Integer getValue() {
+        return this.value;
+    }
 
 }
