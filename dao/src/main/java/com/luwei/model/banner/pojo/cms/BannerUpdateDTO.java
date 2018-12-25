b050753f4ab4cc1b264675a4fc485861bcdf76b8
huanglp <huanglongpeng@talkmoney.cn>
2018-12-25 17:28:54 +0800

    fix dao : 修改Banner相关

--- dao/src/main/java/com/luwei/model/banner/pojo/cms/BannerUpdateDTO.java
+++ dao/src/main/java/com/luwei/model/banner/pojo/cms/BannerUpdateDTO.java
@@ -23,27 +23,27 @@ public class BannerUpdateDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @NotBlank
+    @NotNull(message = "主键ID不能为空")
     @ApiModelProperty(value = "主键ID")
     private Integer bannerId;
 
-    @NotBlank
+    @NotBlank(message = "图片地址不能为空")
     @ApiModelProperty(value = "图片")
     private String picture;
 
-    @NotNull
+    @NotNull(message = "跳转服务ID不能为空")
     @ApiModelProperty(value = "跳转服务ID")
     private Integer jumpId;
 
-    @NotBlank
+    @NotBlank(message = "跳转服务名称不能为空")
     @ApiModelProperty(value = "跳转服务名称")
     private String jumpName;
 
-    @NotNull
+    @NotNull(message = "服务类型不能为空")
     @ApiModelProperty(value = "服务类型 0-课程 1-托管")
     private ServiceTypeEnum serviceType;
 
-    @NotNull
+    @NotNull(message = "权重不能为空")
     @Min(1)
     @ApiModelProperty(value = "权重,越小越大")
     private Integer weight;
@@ -54,11 +54,11 @@ public class BannerUpdateDTO implements Serializable {
     @ApiModelProperty(value = "是否为外链 0-否 1-是")
     private Boolean outsideLink;
 
-    @NotNull
+    @NotNull(message = "轮播图类型不能为空")
     @ApiModelProperty(value = "轮播图类型 0-首页 1-订课 2-点餐")
     private BannerTypeEnum bannerType;
 
-    @NotNull
+    @NotNull(message = "是否显示不能为空")
     @ApiModelProperty(value = "是否显示 0-否 1-是")
     private Boolean display;
 
