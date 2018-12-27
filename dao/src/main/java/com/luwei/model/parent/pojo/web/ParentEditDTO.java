3c28cb70c674dcd1ce7f811b56631d105fc44041
huanglp <huanglongpeng@talkmoney.cn>
2018-12-27 10:02:43 +0800

    feat all : 修改家长信息时，只有联系方式必填

--- dao/src/main/java/com/luwei/model/parent/pojo/web/ParentEditDTO.java
+++ dao/src/main/java/com/luwei/model/parent/pojo/web/ParentEditDTO.java
@@ -19,24 +19,17 @@ public class ParentEditDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "家长姓名")
-    @NotBlank(message = "家长姓名不能为空")
-    private String parentName;
-
     @ApiModelProperty(value = "家长联系方式")
     @NotBlank(message = "家长联系方式不能为空")
     private String phone;
 
+    @ApiModelProperty(value = "家长姓名")
+    private String parentName;
+
     @ApiModelProperty(value = "家庭关系")
-    @NotBlank(message = "家庭关系不能为空")
     private String familyRelation;
 
     @ApiModelProperty(value = "家庭住址")
-    @NotBlank(message = "家庭住址不能为空")
     private String address;
-
-    @ApiModelProperty(value = "家长头像")
-    @NotBlank(message = "家长头像不能为空")
-    private String avatarUrl;
 
 }
