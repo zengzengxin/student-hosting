d9373e31c6079a4c0161c86bb27b71079c36b415
huanglp <huanglongpeng@talkmoney.cn>
2018-12-29 11:29:16 +0800

    feat all : 新增测试类

---
+++ cms/src/test/java/com/luwei/controller/BannerControllerTest.java
@@ -0,0 +1,22 @@
+package com.luwei.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Author: huanglp
+ * Date: 2018-12-29
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BannerControllerTest {
+
+    @Test
+    public void saveBanner() {
+        System.out.println("==========可在此测试==========");
+
+    }
+
+}
