42fd1bc23772ceea4ef51e3ec4fa223779dbe9a8
huanglp <huanglongpeng@talkmoney.cn>
2019-01-07 16:13:15 +0800

    feat all : 01-07 web端 公告

--- web/src/main/java/com/luwei/service/notice/NoticeService.java
+++ web/src/main/java/com/luwei/service/notice/NoticeService.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.luwei.common.exception.MessageCodes;
 import com.luwei.common.util.BeanUtils;
+import com.luwei.common.util.ConversionBeanUtils;
 import com.luwei.model.notice.Notice;
 import com.luwei.model.notice.NoticeMapper;
 import com.luwei.model.notice.pojo.web.NoticeWebVO;
@@ -21,34 +22,23 @@ import org.springframework.util.Assert;
 @Service
 public class NoticeService extends ServiceImpl<NoticeMapper, Notice> {
 
-
     private NoticeWebVO toNoticeVO(Notice notice) {
         NoticeWebVO noticeVO = new NoticeWebVO();
-        BeanUtils.copyNonNullProperties(notice,noticeVO);
-        return  noticeVO;
+        BeanUtils.copyNonNullProperties(notice, noticeVO);
+        return noticeVO;
     }
 
-   public NoticeWebVO findById(Integer id) {
+    public NoticeWebVO findById(Integer id) {
         // 若此id已被逻辑删除,也会返回null
         Notice notice = getById(id);
         Assert.notNull(notice, MessageCodes.NOTICE_IS_NOT_EXIST);
         return toNoticeVO(notice);
     }
 
-
-
-
     public IPage<NoticeWebVO> getNoticePage(Page<Notice> page) {
-        QueryWrapper queryWrapper = new QueryWrapper();
-        return baseMapper.selectPage(page, queryWrapper);
-       /* Notice notice = new Notice();
-        BeanUtils.copyNonNullProperties(noticePageDTO, notice);
-        QueryWrapper<Notice> noticeQueryWrapper = new QueryWrapper<Notice>();
-       // if(noticePageDTO.)
-        noticeQueryWrapper.lambda().between(Notice::getCreateTime,noticePageDTO.getStartTime(), noticePageDTO.getEndTime())
-                .like(Notice::getNoticeTittle,noticePageDTO.getNoticeTittle())
-                .eq(Notice::getNoticeStatus,noticePageDTO.getNoticeStatus());
-        return ConversionBeanUtils.conversionBean(baseMapper.selectPage(page,noticeQueryWrapper), this::toNoticeVO);*/
+        return ConversionBeanUtils.conversionBean(page(page, new QueryWrapper<Notice>().lambda()
+                .eq(Notice::getDisplay, true)
+        ), this::toNoticeVO);
     }
 
 }
