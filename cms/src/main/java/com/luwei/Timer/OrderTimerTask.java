3f112223292cc9f84b3122a3c8ee7275799833c0
huanglp <huanglongpeng@talkmoney.cn>
2019-01-09 11:40:58 +0800

    feat all : 01-09 刷新订单状态的定时器

--- cms/src/main/java/com/luwei/Timer/OrderTimerTask.java
+++ cms/src/main/java/com/luwei/Timer/OrderTimerTask.java
@@ -1,6 +1,5 @@
 package com.luwei.Timer;
 
-import com.luwei.model.hosting.Hosting;
 import com.luwei.service.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,8 +19,7 @@ public class OrderTimerTask {
     @Resource
     private OrderService orderService;
 
-    // @Scheduled(cron = "0 0 2 * * ?")
-    @Scheduled(cron = "0 * * * * ?")
+    @Scheduled(cron = "0 0 2 * * ?")
     private void refreshOrderStatus() {
         log.info("===================刷新订单状态定时任务启动===================");
         log.info("当前时间 {}", LocalDateTime.now());
@@ -38,45 +36,6 @@ public class OrderTimerTask {
         long end = System.currentTimeMillis();
         log.info("当前时间 {} 共耗时{}毫秒", LocalDateTime.now(), String.valueOf(end - start));
         log.info("===================刷新订单状态定时任务结束===================");
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        long start = System.currentTimeMillis();
-
-        Hosting hosting = new Hosting();
-        hosting.setStartTime(LocalDateTime.now());
-        hosting.setEndTime(LocalDateTime.now());
-        System.out.println("hosting.getStartTime() "+hosting.getStartTime());
-        System.out.println("hosting.getEndTime() "+hosting.getEndTime());
-
-        hosting.setStartTime(hosting.getStartTime().withHour(0).withMinute(0).withSecond(0));
-        hosting.setEndTime(hosting.getEndTime().withHour(23).withMinute(59).withSecond(59));
-
-        System.out.println("hosting.getStartTime() "+hosting.getStartTime());
-        System.out.println("hosting.getEndTime() "+hosting.getEndTime());
-
-        long end = System.currentTimeMillis();
-        System.out.println("共耗时" + (end - start));
-
-        /*LocalDateTime time = LocalDateTime.now();
-        System.out.println("LocalDateTime" + time);
-        System.out.println("Date" + new Date());
-        LocalDateTime localDateTime = time.withHour(0).withMinute(0).withSecond(0);
-
-        System.out.println(localDateTime);
-        DateTime dateTime = new DateTime();
-        System.out.println(dateTime);
-        Date start = dateTime.withHourOfDay(0)
-                .withMinuteOfHour(0)
-                .withSecondOfMinute(0)
-                .toDate();
-        System.out.println(start);
-        Date end = dateTime
-                .withHourOfDay(23)
-                .withMinuteOfHour(59)
-                .withSecondOfMinute(59)
-                .toDate();
-        System.out.println(end);*/
     }
 
 }
