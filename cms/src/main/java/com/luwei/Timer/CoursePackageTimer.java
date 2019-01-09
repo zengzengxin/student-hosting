f278363df00c242e68162dc33b870233b8500199
zengzengxin <cengzengxin@talkmoney.cn>
2019-01-09 14:49:46 +0800

    託管定時器，修改課程套餐定時器

--- cms/src/main/java/com/luwei/Timer/CoursePackageTimer.java
+++ cms/src/main/java/com/luwei/Timer/CoursePackageTimer.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
@@ -15,9 +16,15 @@ public class CoursePackageTimer {
     private CoursePackageService coursePackageService;
 
     @Scheduled(cron = "0 0 1 * * ?")
-    //@Scheduled(cron = "0/1 * * * * ?")
     private void coursePackageTimer() {
-        coursePackageService.coursePackageTimer();
+        long start = System.currentTimeMillis();
+        log.info("===================刷新课程套餐是否过期定时任务启动===================");
+        log.info("当前时间 {}", LocalDateTime.now());
+        int i = coursePackageService.coursePackageTimer();
+        log.info("托管刷新为已过期的有{}条", i);
+        long end = System.currentTimeMillis();
+        log.info("当前时间 {} 共耗时{}毫秒", LocalDateTime.now(), String.valueOf(end - start));
+        log.info("===================刷新课程套餐是否过期定时任务结束==================");
     }
 
 }
