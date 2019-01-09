13dce5732aa25cfa12774346d800f83e41f6b986
huanglp <huanglongpeng@talkmoney.cn>
2019-01-09 13:35:50 +0800

    feat all : 01-09 优化课程模块

--- dao/src/main/java/com/luwei/model/coursepackage/CoursePackageMapper.java
+++ dao/src/main/java/com/luwei/model/coursepackage/CoursePackageMapper.java
@@ -2,7 +2,6 @@ package com.luwei.model.coursepackage;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.luwei.model.course.pojo.mini.MyCourseVO;
-import com.luwei.model.coursepackage.pojo.web.CoursePackageWebVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.math.BigDecimal;
@@ -14,9 +13,6 @@ import java.util.List;
  * Date: 2018-12-11
  */
 public interface CoursePackageMapper extends BaseMapper<CoursePackage> {
-
-    //删除
-    List<CoursePackageWebVO> findAllByCourseId(@Param("courseId") Integer courseId);
 
     BigDecimal findMinPriceByCourseId(Integer courseId);
 
