bde79347d834aa9a937cf9f63c8a9d7370369710
huanglp <huanglongpeng@talkmoney.cn>
2019-01-10 11:44:14 +0800

    feat all : 01-10 优化

--- cms/src/main/java/com/luwei/Timer/CoursePackageTimer.java
+++ cms/src/main/java/com/luwei/Timer/CoursePackageTimer.java
@@ -1,12 +1,12 @@
 package com.luwei.Timer;
 
-import com.luwei.common.annotation.TimeCalculateAnnotation;
 import com.luwei.service.coursepackage.CoursePackageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 @Component
 @Slf4j
@@ -15,14 +15,15 @@ public class CoursePackageTimer {
     @Resource
     private CoursePackageService coursePackageService;
 
-    @TimeCalculateAnnotation
     @Scheduled(cron = "0 0 1 * * ?")
     private void coursePackageTimer() {
         long start = System.currentTimeMillis();
         log.info("===================刷新课程套餐是否过期定时任务启动===================");
+        log.info("当前时间 {}", LocalDateTime.now());
         int i = coursePackageService.coursePackageTimer();
         log.info("托管刷新为已过期的有{}条", i);
         long end = System.currentTimeMillis();
+        log.info("当前时间 {} 共耗时{}毫秒", LocalDateTime.now(), String.valueOf(end - start));
         log.info("===================刷新课程套餐是否过期定时任务结束==================");
     }
 
